3fb9b8042fddac54a9ad4692b1bb356fd5d908e2
SandhyaKollati <sandhyarani.kollati@narayanagroup.com>
2022-09-09 16:24:20 +0530

    changes in intervals

--- BarGraphCustomUsingTableView/CustomView.swift
+++ BarGraphCustomUsingTableView/CustomView.swift
@@ -11,30 +11,39 @@ import UIKit
 class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
     
     var dataArray :[BarChartValues] = []
-    var salesValues : [Int] = []
-    var minValue : Int = 1
-    var maxValue : Int = 100
-    var rangeValuesArray : [Int] = []
+    var salesValues : [Double] = [15.5,0.3,16.0,0.6,0.5,1.0]
+    
+    var largestValue : Double = 0
+    var intervalsValue : [Double] = []
+    var labelsArray : [UILabel] = []
+    var secondLabelArray : [UILabel] = []
+    
     
     let tableview = UITableView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         createTableView()
+        creatingIntervalRanges()
+        
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         createTableView()
+        creatingIntervalRanges()
     }
+    
+    var y: Int = 0
     override func layoutSubviews() {
         super.layoutSubviews()
+        print(salesValues[0],"salesvalues")
         let label  = UILabel()
         label.text = "Months"
         label.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
-        label.frame = CGRect(x: 0, y: frame.height/2 - 30, width: 20, height: 60)
+        label.frame = CGRect(x: 0, y: frame.height / 2 - 180, width: 20, height: 60)
         addSubview(label)
         
-        let myview = UIView(frame: CGRect(x: 0, y: frame.height - 120 , width: frame.width, height: 150))
+        let myview = UIView(frame: CGRect(x: 0, y: frame.height - 430 , width: frame.width, height: 150))
         addSubview(myview)
         
         let firstStack = creatingImageView(color: .yellow,  width: myview.frame.width / 3, text: "Correct answers")
@@ -60,6 +69,46 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
         myview.addSubview(secondStackview)
         
     }
+   func creatingIntervals() -> Double
+    {
+        largestValue = salesValues.max() ?? 1.0
+        print(largestValue)
+        print("sandhya")
+        
+        if largestValue < 1
+        {
+            let value = 0.1666666667
+            let y = Double(round(100 * value)/100)
+//            print(y)
+            
+            return y
+        }
+        else if  largestValue > 10
+        {
+            return 5.0
+        }
+        else if largestValue > 4
+        {
+            return  1.0
+        }
+        else{
+           return  0.5
+        }
+         
+    }
+    private func creatingIntervalRanges()
+    {
+        let result = creatingIntervals()
+        print(result,"result")
+        var i = 0.0
+        while i < largestValue
+        {
+            i = i + creatingIntervals()
+            let roundedValue = Double(round(i*100)/100)
+            intervalsValue.append(roundedValue)
+        }
+        print(intervalsValue)
+    }
     func createTableView()
     {
         
@@ -76,10 +125,10 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
         tableview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
         tableview.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         tableview.topAnchor.constraint(equalTo: topAnchor,constant: 50).isActive = true
-        tableview.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -120).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -430).isActive = true
         
-
-       
+        
+        
         
         let createPostNib = UINib(nibName: "TableViewCell", bundle: nil)
         tableview.register(createPostNib, forCellReuseIdentifier: "cell")
@@ -100,7 +149,7 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
         cell.dataLabel.text = dataArray[indexPath.row].month
-        cell.widthConstr.constant = translateWidthValueToXPosition(value: Float(dataArray[indexPath.row].salesValue) / Float(100.0))
+        cell.widthConstr.constant = translateWidthValueToXPosition(value: Float(dataArray[indexPath.row].salesValue) / Float(intervalsValue.last ?? 1.0))
         cell.dataView.backgroundColor = dataArray[indexPath.row].barColour
         cell.mainImageView.backgroundColor = .darkGray
         cell.secondImageView.backgroundColor = dataArray[indexPath.row].salesValueColour
@@ -125,51 +174,27 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
         label.text = "Sales Value"
         headerView.addSubview(label)
         
-        
-        let firstLabel = creatingLabels(text: String(rangeValuesArray[0]), width: 20, alignment: .center)
-        
-        
-        let secondLabel = creatingLabels(text: String(rangeValuesArray[1]), width: 20, alignment: .center)
-        
-        
-        let thirdLabel = creatingLabels(text: String(rangeValuesArray[2]), width: 20, alignment: .center)
-        
-        
-        
-        let fourthLabel = creatingLabels(text: String(rangeValuesArray[3]), width: 20, alignment: .center)
-        
-        
-        let fifthLabel = creatingLabels(text: String(rangeValuesArray[4]), width: 20, alignment: .center)
-        
-        
-        let stackview = UIStackView(arrangedSubviews: [firstLabel,secondLabel,thirdLabel,fourthLabel,fifthLabel])
-        stackview.contentMode = .center
+        for i in 0..<intervalsValue.count
+        {
+            let label = creatingLabels(text: String(intervalsValue[i]), width: translateWidthValueToXPosition(value: Float(creatingIntervals())), alignment: .right)
+            labelsArray.append(label)
+        }
+       
+       let stackview = UIStackView(arrangedSubviews: labelsArray)
         stackview.axis = .horizontal
         stackview.spacing = 0
         stackview.distribution = .fillEqually
-        stackview.frame = CGRect(x: 52, y: 25, width: headerView.frame.width - 71, height: 25)
+        stackview.frame = CGRect(x: 62, y: 25, width: headerView.frame.width - 88.5, height: 25)
         stackview.clipsToBounds = true
         headerView.addSubview(stackview)
-        print("sandhya",stackview.frame.width)
         
-        let secondFirstLabel = creatingLabels(text: "|", width: 20, alignment: .center)
-       
-        
-        let secondSecondLabel = creatingLabels(text: "|", width: 20, alignment: .center)
-        
-        
-        let secondThirdLabel = creatingLabels(text: "|", width: 20, alignment: .center)
-        
-        
-        let secondFourthLabel = creatingLabels(text: "|", width: 20, alignment: .center)
-        
-        
-        let secondFifthLabel = creatingLabels(text: "|", width: 20, alignment: .center)
-        
-        
-        let secondStackview = UIStackView(arrangedSubviews:[secondFirstLabel,secondSecondLabel,secondThirdLabel,secondFourthLabel,secondFifthLabel] )
+        for _ in 0..<intervalsValue.count{
+            let label = creatingLabels(text: "|", width: creatingIntervals(), alignment: .right)
+            secondLabelArray.append(label)
+        }
+        let secondStackview = UIStackView(arrangedSubviews:secondLabelArray)
         secondStackview.axis = .horizontal
-        secondStackview.frame = CGRect(x: 52, y: 50 , width: headerView.frame.width - 75, height: 11)
+        secondStackview.frame = CGRect(x: 52, y: 52 , width: headerView.frame.width - 90, height: 10)
         secondStackview.distribution = .fillEqually
         headerView.addSubview(secondStackview)
         
@@ -184,6 +209,7 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 60
     }
+    
     private func translateWidthValueToXPosition(value : Float) -> CGFloat {
         let width = CGFloat(value) * (frame.width - 92)
         return abs(width)
@@ -205,7 +231,7 @@ class CustomView : UIView, UITableViewDelegate, UITableViewDataSource {
         label.heightAnchor.constraint(equalToConstant: 20).isActive = true
         label.text = text
         label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 12.0)
+        label.font = UIFont.systemFont(ofSize: 11.0)
         label.textColor = UIColor.darkGray
         
         let imageview = UIImageView()
